2ba15142376d284cd782f7480900e94a1f1885b2
MikeWeDev <mikyasnegash2@gmail.com>
2025-08-15 21:32:03 +0300

    adding some space for the card in user page

--- app/admine/users/page.tsx
+++ app/admine/users/page.tsx
@@ -39,7 +39,7 @@ const icons = {
 
 
 const Card = ({ title, value, icon }: { title: string; value: string | number; icon: keyof typeof icons }) => (
-  <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center sm:flex-row sm:items-start w-2/3">
+  <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center sm:flex-row sm:items-start w-3/4">
     <div className="mb-2 sm:mb-0 sm:mr-3">{icons[icon]}</div>
     <div className="text-center sm:text-left">
       <p className="text-gray-500 text-sm truncate">{title}</p>
@@ -170,7 +170,7 @@ export default function UserManagementPage() {
   
     <div className=" mx-auto flex flex-col gap-6">
         {/* Summary Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
           <Card title="Total Registered Users" value={summary.users.toLocaleString()} icon="users" />
           <Card title="Total Games (Overall)" value={`${summary.totalGamesOverall.toLocaleString()} Games`} icon="games" />
           <Card title="Total Account Balance" value={`${summary.totalAccountBalance.toLocaleString()} Birr`} icon="revenue" />
