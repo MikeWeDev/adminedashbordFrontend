9fea84a1c2464c6c29e6144422f41bc2f6510181
MikeWeDev <mikyasnegash2@gmail.com>
2025-08-15 11:52:04 +0300

    tring admine page responsivness

--- app/superadmine/layout.tsx
+++ app/superadmine/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import Sidebar from './Sidbar'
+import Sidebar from './Sidebar'
 import { FaBars } from 'react-icons/fa';
 
 export default function SuperAdminLayout({
