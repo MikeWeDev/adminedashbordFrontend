220228db415a70473b2052c529d5e877f95a3690
MikeWeDev <mikyasnegash2@gmail.com>
2025-08-15 22:46:11 +0300

    fixing the bg balck for layout.tsx

--- app/admine/layout.tsx
+++ app/admine/layout.tsx
@@ -29,7 +29,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
         </div>
 
         {/* Main content */}
-        <main className="flex-1 flex justify-start overflow-x-hidden ">
+        <main className="flex-1 flex justify-start overflow-x-hidden bg-gray-100 ">
           {/* Inner wrapper controlling width */}
           <div className="w-full">
             {children}
