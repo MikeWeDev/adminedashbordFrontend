1d7b7101f80ea428d765dd09a04a40fde7cbc4ee
MikeWeDev <mikyasnegash2@gmail.com>
2025-08-15 12:20:55 +0300

    tring admine page responsivness

--- app/admine/page.tsx
+++ app/admine/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
   }
 
   return (
-    <main className="bg-gray-100 min-h-screen p-4 overflow-hidden border-4 border-red-500 border-solid">
+    <main className="bg-gray-100 min-h-screen p-4 overflow-hidden  border-4 border-red-500 border-solid">
       <div className="max-w-[1200px] mx-auto flex flex-col gap-6">
         
         {/* Summary Cards */}
@@ -146,7 +146,7 @@ export default function Home() {
         </div>
 
         {/* Games Table */}
-        <div className="w-full border-4 border-pink-500 border-solid">
+        <div className="w-full border-4 border-pink-500 border-solid w-2/3">
           <DashboardTable games={games} />
         </div>
       </div>
