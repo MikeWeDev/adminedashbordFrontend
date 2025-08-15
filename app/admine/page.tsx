9370be85ec9b7515bb7524ea226f74b06b21558d
MikeWeDev <mikyasnegash2@gmail.com>
2025-08-15 12:11:15 +0300

    tring admine page responsivness

--- app/admine/page.tsx
+++ app/admine/page.tsx
@@ -61,7 +61,7 @@ const icons = {
 };
 
 const Card = ({ title, value, icon }: { title: string; value: string | number; icon: keyof typeof icons }) => (
-  <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center sm:flex-row sm:items-start w-1/2 ">
+  <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center sm:flex-row sm:items-start w-3/4 ">
     <div className="mb-2 sm:mb-0 sm:mr-3">{icons[icon]}</div>
     <div className="text-center sm:text-left">
       <p className="text-gray-500 text-sm truncate">{title}</p>
