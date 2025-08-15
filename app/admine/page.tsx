a5da78d652e6d116956cae7279f9ce2c38dd501b
MikeWeDev <mikyasnegash2@gmail.com>
2025-08-15 20:43:04 +0300

    fixing responsive issues

--- app/admine/page.tsx
+++ app/admine/page.tsx
@@ -134,9 +134,9 @@ export default function Home() {
   }
 
   return (
-<main className="flex-1  flex items-start justify-center border-4 border-red-500 border-solid ">
+<main className="flex-1 w-[70%] p-4 flex items-start justify-start border-4 border-red-500 border-solid ">
   {/* Inner container with clamp and actual background */}
-  <div className="w-[clamp(250px,70%,800px)] mx-auto bg-gray-100">
+  <div className="w-[clamp(250px,80%,800px)]  bg-gray-100">
     <div className=" mx-auto flex flex-col gap-6">
       
       {/* Summary Cards */}
