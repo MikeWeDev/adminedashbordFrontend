55269fe0508dba3cb7af628e5782c3950e13a1ff
MikeWeDev <mikyasnegash2@gmail.com>
2025-09-22 15:32:23 +0300

    fixing the text color frombalck to white

--- app/superadmine/transcations/page.tsx
+++ app/superadmine/transcations/page.tsx
@@ -182,7 +182,7 @@ export default function PaymentManagementPage() {
               id="date-filter"
               value={selectedDate} 
               onChange={(e) => setSelectedDate(e.target.value)}
-              className="mt-1 block w-full md:w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full md:w-1/3 px-3 py-2 border border-gray-300 text-black rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
 
